417fbc671bbfbe482ebb63591669633eb000188c
TongAn001 <tongan001@TongAn001deMini.lan>
2018-06-05 16:23:41 +0800

    1.0.3

--- DDHTTPClient/DDHTTPClient.h
+++ DDHTTPClient/DDHTTPClient.h
@@ -40,19 +40,19 @@ typedef void (^DDHTTPFailureCallback)(NSError * error);
 typedef NSURL * (^DDHTTPDownloadDestination)(NSURL * targetPath, NSURLResponse * response);
 typedef void (^DDHTTPDownloadCompletion)(NSURLResponse * response, NSURL * filePath, NSError * error);
 
-@class DDHTTPRequest;
-typedef DDHTTPRequest *(^DDHTTPRequestURL)(NSString * url);
-typedef DDHTTPRequest *(^DDHTTPRequestHeader)(NSDictionary * header);
-typedef DDHTTPRequest *(^DDHTTPRequestParams)(NSDictionary * params);
-typedef DDHTTPRequest *(^DDHTTPRequestUploadFile)(NSArray<DDHTTPUploadModel *> * files);
-typedef DDHTTPRequest *(^DDHTTPRequestDownloadDestination)(DDHTTPDownloadDestination callback);
-typedef DDHTTPRequest *(^DDHTTPRequestDownloadCompletion)(DDHTTPDownloadCompletion callback);
-typedef DDHTTPRequest *(^DDHTTPRequestProgress)(DDHTTPProgressCallback callback);
-typedef DDHTTPRequest *(^DDHTTPRequestSuccess)(DDHTTPSuccessCallback callback);
-typedef DDHTTPRequest *(^DDHTTPRequestFailure)(DDHTTPFailureCallback callback);
+@class DDHTTPClient;
+typedef DDHTTPClient *(^DDHTTPRequestURL)(NSString * url);
+typedef DDHTTPClient *(^DDHTTPRequestHeader)(NSDictionary * header);
+typedef DDHTTPClient *(^DDHTTPRequestParams)(NSDictionary * params);
+typedef DDHTTPClient *(^DDHTTPRequestUploadFile)(NSArray<DDHTTPUploadModel *> * files);
+typedef DDHTTPClient *(^DDHTTPRequestDownloadDestination)(DDHTTPDownloadDestination callback);
+typedef DDHTTPClient *(^DDHTTPRequestDownloadCompletion)(DDHTTPDownloadCompletion callback);
+typedef DDHTTPClient *(^DDHTTPRequestProgress)(DDHTTPProgressCallback callback);
+typedef DDHTTPClient *(^DDHTTPRequestSuccess)(DDHTTPSuccessCallback callback);
+typedef DDHTTPClient *(^DDHTTPRequestFailure)(DDHTTPFailureCallback callback);
 
 
-@interface DDHTTPRequest : NSObject
+@interface DDHTTPClient : NSObject
 
 @property (nonatomic,strong,readonly) DDHTTPRequestURL url;
 @property (nonatomic,strong,readonly) DDHTTPRequestHeader header;
@@ -64,7 +64,7 @@ typedef DDHTTPRequest *(^DDHTTPRequestFailure)(DDHTTPFailureCallback callback);
 @property (nonatomic,strong,readonly) DDHTTPRequestSuccess success;
 @property (nonatomic,strong,readonly) DDHTTPRequestFailure failure;
 
-+ (AFHTTPSessionManager *)httpManager;
++ (AFHTTPSessionManager *)httpManager:(void (^)(AFHTTPSessionManager * manager))config;
 + (void)configSerializer:(AFHTTPSessionManager *)manager;
 
 - (NSURLSessionDataTask *)head;
@@ -73,11 +73,8 @@ typedef DDHTTPRequest *(^DDHTTPRequestFailure)(DDHTTPFailureCallback callback);
 - (NSURLSessionDataTask *)upload;
 - (NSURLSessionDownloadTask *)download;
 
-@end
 
-@interface DDHTTPClient : NSObject
-
-+ (DDHTTPRequest *)createRequest;
++ (DDHTTPClient *)createClient;
 
 @end
 
