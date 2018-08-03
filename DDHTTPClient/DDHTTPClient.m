a2639e470d6e14fb71e997ef6117f192d0fe2e72
TongAn001 <tongan001@TongAn001deMac-mini.local>
2018-08-03 09:54:09 +0800

    0.0.7

--- DDHTTPClient/DDHTTPClient.m
+++ DDHTTPClient/DDHTTPClient.m
@@ -171,10 +171,8 @@
 }
 
 + (void)configSerializer:(AFHTTPSessionManager *)manager{
-    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
-    //    manager.requestSerializer = [AFJSONRequestSerializer serializer];
-    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-    //    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
     manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:
                                                          @"text/html",
                                                          @"text/xml",
